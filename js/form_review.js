c08eeacf141420714777ed69c6bb699fafde5aa5
hannastep <hanna.stepanov@gmail.com>
2022-09-26 16:47:25 -0400

    Merge branch 'main' into Task6_final

--- js/form_review.js
+++ js/form_review.js
@@ -1,16 +1,12 @@
 const postsController = new PostsController(0);
 
-
-
-
-
 const newPostFrom = document.querySelector('#newUserForm')
 
 newPostFrom.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const newUserName = document.querySelector("#newUserName")
-    const newUserRecommendation = document.querySelector('#newUserRecommendation')
+    let newUserName = document.querySelector("#newUserName")
+    let newUserRecommendation = document.querySelector('#newUserRecommendation')
     let newUserCountry = document.querySelector('#userCountry')
     if (newUserCountry.value == 'Other Country') {
         newUserCountry = document.querySelector('#userOtherCountry');
@@ -31,54 +27,15 @@ newPostFrom.addEventListener('submit', (event) => {
 
     postsController.addPost(userName, userCountry, userCity, recommendation, userImageUrl);
 
-    /* addPostCard({ 
-        userName: userName, 
-        userCountry: userCountry, 
-        userCity: userCity, 
-        recommendation: recommendation, 
-        userImageUrl: userImageUrl })
- */
 
-        /*
     //clear the form
-    newUserNameInput.value = '';
+    newUserName.value = '';
     newUserRecommendation.value = '';
     newUserCountry.value = 'Other Country';
     newUserCity.value = 'Other City';
     userImageUrl.value = ''
-*/
-}); 
 
 
 
-
-/*
-function addPostCard(post) {
-    const itemHTML = '<div class="card" style="width: 20rem;">\n' +
-        '    <img src="' + post.img + '" width="300" height="250"  alt="product image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">' + post.name + '</h5>\n' +
-        '        <p class="card-text">' + post.recommendation + '</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
-    const postsContainer = document.getElementById("list-posts");
-    postsContainer.innerHTML += itemHTML;
-}
-
-
-addPostCard({
-    'name': 'Yiran',
-    'img': 'https://i.natgeofe.com/n/45936334-bdc9-4659-88dd-6f380d5a2b76/2030.jpg?w=636&h=477',
-    'recommendation': 'Tourist Information: Arch National Park'
 
 });
-
-addPostCard({
-    'name': 'Yiran',
-    'img': 'https://a.cdn-hotels.com/gdcs/production126/d349/d2422886-1662-43cb-a356-4087bdbb59f8.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-    'recommendation': 'Tourist Information: Yosemeti National Park'
-
-})
-*/
